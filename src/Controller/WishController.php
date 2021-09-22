cf4d2add6201993ad58ffef52a19559ca9aeae4e
MaximeRavard <maxime.ravard.projects@gmail.com>
2021-09-22 10:18:09 +0200

    -Add Wish and delete wish

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +33,55 @@ class WishController extends AbstractController
         return $this->render('wish/wishDetail.html.twig', [
             'wishResult' => $wish
         ]);
+    }
+
+    /**
+     * @Route("/wishRemove/{id}", name="wishRemove")
+     */
+    public function wishRemove(Wish $wish, EntityManagerInterface $em): Response
+    {
+        $em->remove($wish);
+        $em->flush();
+        return $this->redirectToRoute('wishlist');
+    }
+
+    /**
+     * @Route("/wishAddDure/", name="wishAddDure")
+     */
+    public function wishAddDure(): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $wish = new Wish();
+        $wish->setTitle('Avoir une tesla');
+        $wish->setAuthor('Maxime');
+        $wish->setDescription('Dinguerie la teslaaaa');
+        $wish->setDateCreated(new \DateTime('now'));
+        $em->persist($wish);
+        $em->flush();
+        return $this->redirectToRoute('wishlist');
+
+    }
+
+    /**
+     * @Route("/wishAdd/", name="wishAdd")
+     */
+    public function wishAdd(Request $request): Response
+    {
+        $wish = new Wish();
+        $formWish = $this->createForm(WishType::class, $wish);
+
+        $formWish->handleRequest($request);
+        if($formWish->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($wish);
+            $em->flush();
+            return $this->redirectToRoute('wishlist');
+
+        }
+        return $this->render('wish/ajouter.html.twig', [
+            'wishForm' => $formWish->createView()
+        ]);
+
     }
 
 }
